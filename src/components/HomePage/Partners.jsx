d8b7ab939a857bb1d0a49986b99003ee72a02139
Anas Ardah <anas.m.ardah@gmail.com>
2024-12-31 21:45:06 +0300

    major bug fixes

--- src/components/HomePage/Partners.jsx
+++ src/components/HomePage/Partners.jsx
@@ -1,5 +1,6 @@
 // Partners.jsx
 import { Container, Row, Col } from 'react-bootstrap';
+import { motion } from 'framer-motion';
 import '../../css/HomePage/Partners.css';
 
 // Import your partner logos
@@ -11,48 +12,79 @@ import minrray from '../../assets/images/partners/logo-minrray.png';
 import visionic from '../../assets/images/partners/visonic-logo.jpg';
 
 const Partners = () => {
+    const containerVariants = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.1
+            }
+        }
+    };
+
+    const itemVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.5
+            }
+        }
+    };
+
     return (
-        <section className="partners-section py-5">
+        <section className="par-partners-section py-5">
             <Container>
                 <Row className="align-items-center">
                     {/* Left Column - Text Content */}
-                    <Col lg={4} className="partners-content" data-aos="fade-right">
-                        <div className="section-header">
-                            <span className="subtitle">Our Business Partners</span>
-                            <h2 className="title">
-                                Partners with <span className="highlight">Global</span><br />
-                                <span className="highlight">Companies</span>
+                    <Col lg={4} className="par-partners-content">
+                        <motion.div
+                            initial={{ opacity: 0, x: -50 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            viewport={{ once: true }}
+                            transition={{ duration: 0.5 }}
+                            className="par-section-header"
+                        >
+                            <span className="par-subtitle">Our Business Partners</span>
+                            <h2 className="par-title">
+                                Partners with <span className="par-highlight">Global</span><br />
+                                <span className="par-highlight">Companies</span>
                             </h2>
-                            <p className="description">
+                            <p className="par-description">
                                 We've been lucky to collaborate with a long list of international 
                                 partners, located in and out of the country. Thanks to them we have 
                                 grown as professionals.
                             </p>
-                        </div>
+                        </motion.div>
                     </Col>
 
                     {/* Right Column - Partner Logos */}
-                    <Col lg={8} data-aos="fade-left">
-                        <div className="partners-grid">
-                            <div className="partner-logo" data-aos="fade-up" data-aos-delay="100">
-                                <img src={maxhub} alt="Maxhub" />
-                            </div>
-                            <div className="partner-logo" data-aos="fade-up" data-aos-delay="200">
-                                <img src={huawei} alt="Huawei" />
-                            </div>
-                            <div className="partner-logo" data-aos="fade-up" data-aos-delay="300">
-                                <img src={schneider} alt="Schneider Electric" />
-                            </div>
-                            <div className="partner-logo" data-aos="fade-up" data-aos-delay="400">
-                                <img src={hikvision} alt="Hikvision" />
-                            </div>
-                            <div className="partner-logo" data-aos="fade-up" data-aos-delay="500">
-                                <img src={minrray} alt="Minrray" />
-                            </div>
-                            <div className="partner-logo" data-aos="fade-up" data-aos-delay="600">
-                                <img src={visionic} alt="Visionic" />
-                            </div>
-                        </div>
+                    <Col lg={8}>
+                        <motion.div
+                            className="par-partners-grid"
+                            variants={containerVariants}
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true }}
+                        >
+                            {[
+                                { src: maxhub, alt: "Maxhub" },
+                                { src: huawei, alt: "Huawei" },
+                                { src: schneider, alt: "Schneider Electric" },
+                                { src: hikvision, alt: "Hikvision" },
+                                { src: minrray, alt: "Minrray" },
+                                { src: visionic, alt: "Visionic" }
+                            ].map((partner, index) => (
+                                <motion.div
+                                    key={index}
+                                    className="par-partner-logo"
+                                    variants={itemVariants}
+                                >
+                                    <img src={partner.src} alt={partner.alt} />
+                                </motion.div>
+                            ))}
+                        </motion.div>
                     </Col>
                 </Row>
             </Container>
