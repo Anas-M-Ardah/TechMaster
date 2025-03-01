ad6752822957f44b0ede1f8dcf41d5a14ba55006
Anas Ardah <anas.m.ardah@gmail.com>
2025-03-01 10:45:40 +0300

    reverting the to the old version

--- src/components/Common/LoadingSpinner.jsx
+++ src/components/Common/LoadingSpinner.jsx
@@ -2,26 +2,10 @@
 import React from 'react';
 import '../../css/Common/LoadingSpinner.css';
 
-const LoadingSpinner = ({ progress }) => {
+const LoadingSpinner = () => {
   return (
     <div className="loading-container">
-      <div className="loading-content">
-        <div className="spinner"></div>
-        <div className="progress-bar">
-          <div 
-            className="progress-fill"
-            style={{ width: `${progress}%` }}
-          ></div>
-        </div>
-        <div className="loading-text">
-          Loading... {progress}%
-        </div>
-        {progress < 100 && (
-          <div className="loading-message">
-            Please wait while we load the content...
-          </div>
-        )}
-      </div>
+      <div className="spinner"></div>
     </div>
   );
 };
